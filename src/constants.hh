a8d7e4b8b292a38a13067700821b04c077fce7ea
Griffin Dube <gdube@g.clemson.edu>
2021-06-21 10:18:03 -0400

    Removing unnecessary constants.cc file

--- src/constants.hh
+++ src/constants.hh
@@ -11,23 +11,23 @@
 #include <cstddef>
 
 // dims_L16
-extern const size_t DIM0;
-extern const size_t DIM1;
-extern const size_t DIM2;
-extern const size_t DIM3;
-extern const size_t nBLK0;
-extern const size_t nBLK1;
-extern const size_t nBLK2;
-extern const size_t nBLK3;
-extern const size_t nDIM;
-extern const size_t LEN;
-extern const size_t CAP;
-extern const size_t RADIUS;
+const size_t DIM0   = 0;
+const size_t DIM1   = 1;
+const size_t DIM2   = 2;
+const size_t DIM3   = 3;
+const size_t nBLK0  = 4;
+const size_t nBLK1  = 5;
+const size_t nBLK2  = 6;
+const size_t nBLK3  = 7;
+const size_t nDIM   = 8;
+const size_t LEN    = 12;
+const size_t CAP    = 13;
+const size_t RADIUS = 14;
 
 // ebs_L4
-extern const size_t EB;
-extern const size_t EBr;
-extern const size_t EBx2;
-extern const size_t EBx2_r;
+const size_t EB     = 0;
+const size_t EBr    = 1;
+const size_t EBx2   = 2;
+const size_t EBx2_r = 3;
 
 #endif
